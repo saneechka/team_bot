1cb2e77ac85ec97d709857543dba26cb18d0a0ed
saneechka <alexandro.dev.work@gmail.com>
2025-06-13 18:04:42 +0300

    added roles

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,30 +1,34 @@
 package model
 
-import "slices"
-
-import "time"
-
+import (
+	"time"
+)
 
 type Message struct {
-	ID        int64  `json:"id"`
-	ChatID    int64  `json:"chat_id"`
-	Text      string `json:"text"`
-	UserID    int64  `json:"user_id"`
-	Username  string `json:"username"`
-	Timestamp time.Time  `json:"timestamp"`
-	Type      string `json:"type"`
+	ID        int64     `json:"id"`
+	ChatID    int64     `json:"chat_id"`
+	Text      string    `json:"text"`
+	UserID    int64     `json:"user_id"`
+	Username  string    `json:"username"`
+	Timestamp time.Time `json:"timestamp"`
+	Type      string    `json:"type"`
 }
-
 
 type User struct {
-	ID        int64     `json:"id"`
-	Username  string    `json:"username"`
-	ChatID    int64     `json:"chat_id"`
-	IsAdmin   bool      `json:"is_admin"`
+	ID          int64     `json:"id"`
+	Username    string    `json:"username"`
+	ChatID      int64     `json:"chat_id"`
+	CreatedTime time.Time `json:"created_time"`
+	IsAdmin     bool      `json:"is_admin"`
 }
 
-
+// IsTelegramAdmin проверяет, является ли пользователь администратором
+// на основе списка административных username
 func IsTelegramAdmin(username string, adminUsernames []string) bool {
-	return slices.Contains(adminUsernames, username)
+	for _, adminUsername := range adminUsernames {
+		if username == adminUsername {
+			return true
+		}
+	}
+	return false
 }
-
